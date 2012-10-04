aa25807fba06f8043e7dc60c86d0bf6322b747da
mishapuzanov <misha.puzanov@gmail.com>
2012-10-04 22:10:39 +0700

    Fixes for RandomActors

--- misc/src/main/scala/tests/Exercise1.scala
+++ misc/src/main/scala/tests/Exercise1.scala
@@ -52,7 +52,33 @@ class SampleTest {
     for (x <- 0 until 10) {
       val m = new scala.collection.mutable.HashMap[Int, Int];
       var i = 0;
-      while (i < 100000) { i = i + 1; m.put(i, i); };
+      while (i < 100000) {
+        i = i + 1;
+        m.put(i, i);
+        if (i % 2 == 0) {
+          val k = math.sqrt(i)
+          i += k.toInt
+          i -= k.toInt
+        }
+      }
+    }
+    var end = System.currentTimeMillis();
+    println(end - start);
+  }
+
+  @Test def perfTest11 {
+    var start = System.currentTimeMillis();
+    for (x <- 0 until 10) {
+      var m = new scala.collection.immutable.HashMap[Int, Int];
+      var i = 0;
+      while (i < 100000) {
+        i = i + 1; m += (i -> i);
+        if (i % 2 == 0) {
+          val k = math.sqrt(i)
+          i += k.toInt
+          i -= k.toInt
+        }
+      };
     }
     var end = System.currentTimeMillis();
     println(end - start);
@@ -63,7 +89,15 @@ class SampleTest {
     for (x <- 0 until 10) {
       val m = new java.util.HashMap[Int, Int];
       var i = 0;
-      while (i < 100000) { i = i + 1; m.put(i, i); };
+      while (i < 100000) {
+        i = i + 1;
+        m.put(i, i);
+        if (i % 2 == 0) {
+          val k = math.sqrt(i)
+          i += k.toInt
+          i -= k.toInt
+        }
+      }
     }
     var end = System.currentTimeMillis();
     println(end - start);
@@ -74,7 +108,15 @@ class SampleTest {
     for (x <- 0 until 10) {
       val m = new java.util.concurrent.ConcurrentHashMap[Int, Int];
       var i = 0;
-      while (i < 100000) { i = i + 1; m.put(i, i); };
+      while (i < 100000) {
+        i = i + 1
+        m.put(i, i)
+        if (i % 2 == 0) {
+          val k = math.sqrt(i)
+          i += k.toInt
+          i -= k.toInt
+        }
+      }
     }
     var end = System.currentTimeMillis();
     println(end - start);
@@ -82,7 +124,17 @@ class SampleTest {
 
   @Test def sets {
     val x = new TreeSet[Int]() ++ List(14, 5, 3, 4)
-    println(x.toList)    
+    println(x.toList)
+  }
+
+  @Test
+  def sql {
+    val x = 10
+    val q = """
+      SELECT * FROM persons
+      WHERE id = '{x}'
+    """
+    println("q = " + q)
   }
 
 }
--- misc/src/main/scala/tests/actors/RandomActors.scala
+++ misc/src/main/scala/tests/actors/RandomActors.scala
@@ -1,68 +1,97 @@
 package tests.actors
 
 import akka.actor.{ Actor, ActorRef, Props }
+import akka.pattern._
 import org.junit._
 import scala.util.Random
+import akka.actor.PoisonPill
+import akka.actor.ActorSystem
 import scala.collection.mutable.BitSet
+import akka.dispatch.Future
+import akka.dispatch.Await
+import akka.util.duration._
+
+case class Generate(begin: Int, count: Int)
+case class GeneratedChunk(begin: Int, chunk: List[Int])
+case class Sum(begin: Int, chunk: List[Int])
+case class SumResult(begin: Int, sum: Long)
+
+class SummingActor() extends Actor {
+  def receive = {
+    case Sum(begin, chunk) => {
+      val sum = chunk.foldLeft(0)(_ + _)
+      sender ! SumResult(begin, sum)
+    }
+    case m @ _ => println("Wrong! " + m)
+  }
+}
+
+class GeneratorActor() extends Actor {
+  def receive = {
+    case Generate(begin, count) => {
+      val r = new Random
+      val chunk = (0 until count).map(x => r.nextInt).toList
+      sender ! GeneratedChunk(begin, chunk)
+    }
+    case m @ _ => println("Wow! " + m)
+  }
+}
+
+class DispatchActor extends Actor {
+  val N = 100000
+  val parallelFactor = 10;
+  val chunkSize = N / parallelFactor
+  val values = new Array[Int](N)
+  val generated = new BitSet(parallelFactor)
+
+  var totalSum = 0L;
+  var responseCount = 0;
+
+  var generators = List[ActorRef]();
+  var summingActors = List[ActorRef]();
+
+  override def preStart() = {
+    generators = (0 until parallelFactor).map(i => {
+      val generator = context.actorOf(Props[GeneratorActor], name = "generator" + i)
+      generator ! Generate(chunkSize * i, chunkSize)
+      generator
+    }).toList
+  }
+
+  def receive = {
+    case GeneratedChunk(begin, chunk) => {
+      val summingActor = context.actorOf(Props[SummingActor], name = "summingActor" + begin / chunkSize)
+      summingActor ! Sum(begin, chunk)
+      summingActors = summingActor :: summingActors
+    }
+    case SumResult(begin, sum) => {
+      println("begin = " + begin + ",  sum = " + sum)
+      totalSum += sum
+      responseCount += 1
+      if (responseCount == parallelFactor) {
+        println("totalSum = " + totalSum)
+        die
+      }
+    }
+
+    case m @ _ => println("Oops " + m)
+  }
+
+  def die = {
+    generators.foreach(_ ! PoisonPill)
+    summingActors.foreach(_ ! PoisonPill)
+    generators.map(gracefulStop(_, 5 seconds)(context.system))
+    summingActors.map(gracefulStop(_, 5 seconds)(context.system))
+    self ! PoisonPill
+  }
+}
 
 class RandomActors {
-
-  case class Generate(begin: Int, count: Int)
-  case class GeneratedChunk(begin: Int, chunk: List[Int])
-  case class Sum(begin: Int, chunk: List[Int])
-  case class SumResult(begin: Int, sum: Long)
-
-  class SummingActor() extends Actor {
-    def receive = {
-      case Sum(begin, chunk) => {
-        val sum = chunk.foldLeft(0)(_ + _)
-        sender ! SumResult(begin, sum)
-      }
-      case _ => println("Wow!")
-    }
-  }
-
-  class GeneratorActor() extends Actor {
-    def receive = {
-      case Generate(begin, count) => {
-        val r = new Random
-        val chunk = (0 until count).map(x => r.nextInt).toList
-        sender ! GeneratedChunk(begin, chunk)
-      }
-      case _ => println("Wow!")
-    }
-  }
-
-  class DispatchActor extends Actor {
-    val N = 1000000
-    val parallelFactor = 10;
-    val chunkSize = N / parallelFactor
-    val values = new Array[Int](N)
-    val generated = new BitSet(parallelFactor)
-
-    var totalSum = 0L;
-
-    override def preStart() = {
-      (0 until parallelFactor).foreach(i => {
-        val generator = context.actorOf(Props[GeneratorActor], name = "generator" + i)
-        generator ! Generate(chunkSize * i, chunkSize)
-      })
-    }
-
-    def receive = {
-      case GeneratedChunk(begin, chunk) => {
-        val generator = context.actorOf(Props[GeneratorActor], name = "summingActor" + begin / chunkSize)
-        generator ! Sum(begin, chunk)
-      }
-      case SumResult(begin, sum) => {
-        totalSum += sum
-      }
-      case _ => println("Wow 2!")
-    }
-  }
-
-  @Test def testMean {
-
+  @Test def testRandom {
+    val actorSystem = ActorSystem("RandomActors")
+    val dispatcher = actorSystem.actorOf(Props[DispatchActor], name = "dispatcher")
+    val s = gracefulStop(dispatcher, 10 seconds)(actorSystem)
+    Await.result(s, 11 seconds)
   }
 
 }
