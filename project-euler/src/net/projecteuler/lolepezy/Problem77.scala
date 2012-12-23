0a4e1450c769e50ddb6ff276da1f70b00eb5740c
mikhailp <misha.puzanov@gmail.com>
2012-12-23 22:54:54 +0700

    Clean up

--- project-euler/src/net/projecteuler/lolepezy/Problem77.scala
+++ project-euler/src/net/projecteuler/lolepezy/Problem77.scala
@@ -5,19 +5,15 @@ import org.junit.Test
 class Problem77 {
   @Test
   def solve() {
-    //    println(Primes.primes(10))
-    //    println(getDecompositions(2, 10))
-    println(decompose(10))
-    println(decompose(20))
-
-    for (n <- 10 to 100) {
-      if (decompose(n).length > 5000)
-        println("answer = " + n)
-    }
+    var (n, d) = (20, 0)
+    do {
+      n += 1
+      d = decompose(n).length
+    } while (d < 5000)
+    println("n = " + n + ", d = " + d)
   }
 
   def decompose(n: Long) = {
-
     val primes = Primes.primes(n);
     def decompose1(p: Long, k: Long): List[Option[List[Long]]] = {
       primes.withFilter(_ >= p).flatMap(i => {
@@ -25,15 +21,12 @@ class Problem77 {
         if (z == 0) List(Some(List(k)))
         else if (z > 1) {
           decompose1(i, z).collect({
-            case Some(x) => Some({ val p: List[Long] = i :: x; p })
+            case Some(x) => Some[List[Long]](i :: x)
           })
         } else List(None)
       })
     }
 
-    decompose1(primes.head, n).collect({
-      case Some(dec) => dec
-    }).toList
-
+    decompose1(primes.head, n).collect({ case Some(dec) => dec }).toList
   }
 }
